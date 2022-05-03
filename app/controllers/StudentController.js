606470866943565ea83b86ac8a396c0f4d1fd1ef
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-05-02 23:13:00 -0500

    Adición del método getStudentsByCertification

--- app/controllers/StudentController.js
+++ app/controllers/StudentController.js
@@ -11,6 +11,11 @@ class StudentController{
         const students = Reader.readJsonFile("./data/visualpartners.json");
         return StudentService.filterByCreditsGreaterThan(students, credits);
     }
+
+    static getStudentsByCertification(haveCertification){
+        const students = Reader.readJsonFile("./data/visualpartners.json");
+        return StudentService.filterByCertification(students, haveCertification);
+    }
 }
 
 module.exports = StudentController;
