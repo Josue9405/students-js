265410dd519899c534bd7a29b8ed36b28340d288
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-05-02 22:42:10 -0500

    Adición del método filterByName

--- app/services/StudentService.js
+++ app/services/StudentService.js
@@ -1,4 +1,16 @@
 class StudentService{
+    static filterByName(students, name){
+        if(Array.isArray(students) && students.length > 0){
+            if (name == "all"){
+                return students;
+            }else{
+                return students.filter((student) => student.name == name);
+            }
+        }else{
+            return "No hay estudiantes";
+        }
+    }
+
     static filterByCertification(students, haveCertification){
         if(Array.isArray(students) && students.length > 0){
             return students.filter((student) => student.haveCertification == haveCertification);
