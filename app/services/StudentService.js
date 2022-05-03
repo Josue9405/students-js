76af6b9d62dd19fc886c86bbe617a4922e1a4c0f
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-05-02 23:51:12 -0500

    Corrección a filtrado por certificación

--- app/services/StudentService.js
+++ app/services/StudentService.js
@@ -1,4 +1,16 @@
+Boolean.parse = function (str) {
+    switch (str.toLowerCase ()) {
+        case "true":
+            return true;
+        case "false":
+            return false;
+        default:
+            throw new Error ("Boolean.parse: No se puede convertir la cadena a Booleano");
+    }
+};
+
 class StudentService{
+
     static filterByName(students, name){
         if(Array.isArray(students) && students.length > 0){
             if (name == "all"){
@@ -13,7 +25,7 @@ class StudentService{
 
     static filterByCertification(students, haveCertification){
         if(Array.isArray(students) && students.length > 0){
-            return students.filter((student) => student.haveCertification == haveCertification);
+            return students.filter((student) => student.haveCertification == Boolean.parse(haveCertification));
         }else{
             return "No hay estudiantes";
         }
