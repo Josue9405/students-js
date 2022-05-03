0ccd9af303b8dc90821fc7eca30673c7fa8ea736
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-05-02 21:40:26 -0500

    Corrección de prueba unitaria Reader

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,15 +2,15 @@ const Reader = require("./../../app/utils/reader")
 
 describe("Prueba de unidad de para Reader", () => {
     test("1) Leyendo un archivo con lista vacía", () => {
-        const explorer = JSONReader.readJsonFile("./test/data/empty.test.json");
+        const explorer = Reader.readJsonFile("./test/data/empty.test.json");
         expect(explorer.length).toBe(0);
     });
     test("2) Leyendo un archivo en blanco", () => {
-        const explorer = JSONReader.readJsonFile("./test/data/blank.test.json");
+        const explorer = Reader.readJsonFile("./test/data/blank.test.json");
         expect(explorer).toBe("Error de lectura, archivo en blanco");
     });
     test("3) Archivo inexistente", () => {
-        const explorer = JSONReader.readJsonFile("./test/data/explorers_notfound.test.json");
+        const explorer = Reader.readJsonFile("./test/data/explorers_notfound.test.json");
         expect(explorer).toBe("No se encontró el archivo");
     });
 });
